e7a88fe34d87c500bd6ab54d18ded0a16ace5d7b
mikhailandreev <andreemic@gmail.com>
2023-09-01 18:13:21 +0200

    extract_frames script

---
+++ preprocessing/extract_frames.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 VIDEO_FILE_NAME FOLDER_PATH"
+    exit 1
+fi
+
+VIDEO_FILE_NAME="$1"
+FOLDER_PATH="$2"
+
+# Create the frames directory if it doesn't exist
+mkdir -p "${FOLDER_PATH}/frames"
+
+# Run the ffmpeg command
+ffmpeg -i "$VIDEO_FILE_NAME" -vf "fps=24" "${FOLDER_PATH}/frames/%05d.jpg"
