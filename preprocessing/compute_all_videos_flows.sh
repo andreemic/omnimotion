7e6f6ac91f51c92b851570e6f79f215d76027b1d
Mikhail Andreev <andreemic@gmail.com>
2023-09-09 11:22:15 +0000

    script to precompute all flows

---
+++ preprocessing/compute_all_videos_flows.sh
@@ -0,0 +1,5 @@
+python  main_processing.py --data_dir ../../omnimotion_videos/bmx_short/ --chain
+python  main_processing.py --data_dir ../../omnimotion_videos/cat_walk/ --chain
+python  main_processing.py --data_dir ../../omnimotion_videos/dancers_short --chain
+python  main_processing.py --data_dir ../../omnimotion_videos/monkey_short --chain
+python  main_processing.py --data_dir ../../omnimotion_videos/violin --chain
